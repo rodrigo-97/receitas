f99feeb087a3a0f53173b5f81ec785e7e0d2199c
Rodrigo <rodmatheus99@gmail.com>
2021-07-22 22:46:02 -0300

    add criar e alterar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def index
-    @recipes = Recipe.all
-  end
-
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,39 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find_by(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find_by(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipes_path
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to recipes_path
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+  end
 end
